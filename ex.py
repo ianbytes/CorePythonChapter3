923c3e17cc2cbf6d94c3a21e70c909825bb3256b
JOHNSONI <Ian.Johnson@JRJNET.com>
2019-09-04 22:52:55 -0400

    Chapter3 programming notes

---
+++ ex.py
@@ -0,0 +1,24 @@
+#program with two functions
+def add(x, y):
+    """
+    This function takes two numbers and finds their sum
+    It displays the sum as a result
+    :param x:
+    :param y:
+    :return:
+    """
+
+    print("Sum: ", (x+y))
+
+
+def message():
+    '''
+    This function displays a message.
+    This is a welcome message to the user.
+    :return:
+    '''
+
+    print("Welcome to Python")
+
+add(10,25)
+message()
---
+++ slice.py
@@ -0,0 +1,7 @@
+s = "Welcome to Core Pythong"
+print(s)
+print(s[0])
+print(s[3:7])
+print(s[11:])
+print(s[-1])
+print(s*2)
